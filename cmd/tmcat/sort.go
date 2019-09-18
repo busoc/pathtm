af78cfedf09d19ed9fe15e368f99ee9799c43597
nico <brun.nicolas21@gmail.com>
2019-09-18 14:15:27 +0200

    a writer type to write packets per apid

--- cmd/tmcat/sort.go
+++ cmd/tmcat/sort.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/busoc/pathtm"
@@ -41,6 +41,76 @@ func runMerge(cmd *cli.Command, args []string) error {
 	})
 }
 
+type writer struct {
+	format   rt.Formatter
+	interval time.Duration
+	writers  map[uint16]*os.File
+	times    map[uint16]time.Time
+}
+
+func NewWriter(str string, interval time.Duration) (*writer, error) {
+	f, err := rt.Parse(str)
+	if err != nil {
+		return nil, err
+	}
+	w := writer{
+		format:   f,
+		interval: interval,
+		writers:  make(map[uint16]*os.File),
+		times:    make(map[uint16]time.Time),
+	}
+	return &w, nil
+}
+
+func (w *writer) Close() error {
+	var err error
+	for _, c := range w.writers {
+		e := c.Close()
+		if err == nil && e != nil {
+			err = e
+		}
+	}
+	return err
+}
+
+func (w *writer) WritePacket(p pathtm.Packet) error {
+	apid := p.Apid()
+	when := p.Timestamp().Truncate(w.interval)
+
+	stamp := w.times[apid]
+	if stamp.IsZero() {
+		w.times[apid], stamp = when, when
+
+		pi := rt.PacketInfo{
+			Pid:  int(apid),
+			Sid:  int(p.ESAHeader.Sid),
+			When: when,
+		}
+		file := w.format.Format(pi)
+		if err := os.MkdirAll(filepath.Dir(file), 0755); err != nil {
+			return err
+		}
+		wc, err := os.Create(file)
+		if err != nil {
+			return err
+		}
+		w.writers[apid] = wc
+	}
+	if delta := when.Sub(stamp); delta >= w.interval {
+		if err := w.writers[apid].Close(); err != nil {
+			return err
+		}
+		delete(w.times, apid)
+		delete(w.writers, apid)
+		return w.WritePacket(p)
+	}
+	buf, err := p.Marshal()
+	if err == nil {
+		_, err = w.writers[apid].Write(buf)
+	}
+	return err
+}
+
 func runTake(cmd *cli.Command, args []string) error {
 	var (
 		apid     = cmd.Flag.Int("p", 0, "apid")
@@ -58,23 +128,21 @@ func runTake(cmd *cli.Command, args []string) error {
 	if err != nil {
 		return err
 	}
-	defer mr.Close()
 
-	var (
-		d = pathtm.NewDecoder(rt.NewReader(mr), pathtm.WithApid(*apid))
-		// ws = make(map[int]io.WriteCloser)
-		when time.Time
-	)
+	ws, err := NewWriter(cmd.Flag.Arg(0), *interval)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		mr.Close()
+		ws.Close()
+	}()
+	d := pathtm.NewDecoder(rt.NewReader(mr), pathtm.WithApid(*apid))
 	for {
-		switch _, w, err := d.Marshal(); err {
+		switch p, err := d.Decode(true); err {
 		case nil:
-			w = w.Truncate(*interval)
-			if when.IsZero() {
-				when = w
-			}
-			if delta := w.Sub(when); delta >= *interval {
-				fmt.Println("write", when, delta)
-				when = w
+			if err := ws.WritePacket(p); err != nil {
+				return err
 			}
 		case io.EOF:
 			return nil
